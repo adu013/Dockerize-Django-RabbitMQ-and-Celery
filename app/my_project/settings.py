43c5ea55556ab3ba042b24b0a04bb5bf0057b64a
NSEARCH - Arindam Dutta <arindamdutta132@gmail.com>
2023-11-09 23:34:38 +0800

    Updated settings.py file with CELERY section

--- app/my_project/settings.py
+++ app/my_project/settings.py
@@ -121,3 +121,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Celery
+CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672/'
